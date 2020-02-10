3d6afeac6fca6dd67fdb4c71db0892d33f289bff
Karen Daniela <kdaniimora@gmail.com>
2020-02-10 19:39:45 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,26 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  # Class variables 
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+  end
+  
+  def self.count 
+    p @@count
+  end
+  
+  def self.artists
+    p @@artists.uniq
+  end 
+  
+  def self.genres 
+    p @@genres.uniq 
+  end
